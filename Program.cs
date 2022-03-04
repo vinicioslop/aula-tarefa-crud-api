1c8b06127abc2942e9f473aba84971a074fb5251
vinicioslop <vinicioslop7@hotmail.com>
2022-03-03 22:40:52 -0300

    Finaliza aula de tarefa api

--- Program.cs
+++ Program.cs
@@ -37,4 +37,54 @@ app.MapGet("/api/tarefas", ([FromServices] tarefasContext _db) =>
     return Results.Ok(_db.Tarefa.ToList<Tarefa>());
 });
 
+app.MapGet("/api/tarefas/{id}", (
+    [FromServices] tarefasContext _db,
+    [FromRoute] int id
+) =>
+{
+    // BUCAS PELO ID QUE ESTÁ NA ROTA
+    var tarefa = _db.Tarefa.Find(id);
+
+    // ENCONTROU
+    if (tarefa == null)
+    {
+        // NÃO ENCONTROU
+        // RETORNAR 404
+        return Results.NotFound();
+    }
+
+    // ENCONTROU
+    // RETORNAR 200 E OS DADOS DA TAREFA
+    return Results.Ok(tarefa);
+});
+
+app.MapPost("/api/tarefas", (
+    [FromServices] tarefasContext _db,
+    [FromBody] Tarefa novaTarefa
+) =>
+{
+    if (String.IsNullOrEmpty(novaTarefa.Descricao))
+    {
+        return Results.BadRequest(new { mensagem = "Informe uma descrição." });
+    }
+
+    if (novaTarefa.Concluida)
+    {
+        return Results.BadRequest(new { mensagem = "Cadastre uma tarefa como pendente." });
+    }
+
+    var tarefa = new Tarefa
+    {
+        Descricao = novaTarefa.Descricao,
+        Concluida = novaTarefa.Concluida,
+    };
+
+    _db.Tarefa.Add(tarefa);
+    _db.SaveChanges();
+
+    string urlTarefaCriada = $"/api/tarefas/{tarefa.Id}";
+
+    return Results.Created(urlTarefaCriada, tarefa);
+});
+
 app.Run();
